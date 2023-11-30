894a06b5c106e6727fa7e12877df688f963e4f44
annasve <svejdovaam@gmail.com>
2023-11-30 02:13:25 +0100

    create Places gallery - basic version

--- src/pages/PlacesPage/index.jsx
+++ src/pages/PlacesPage/index.jsx
@@ -1,5 +1,6 @@
 import './style.css';
 import { useEffect } from 'react';
+import SwedenImg from './img/sweden-640.jpg';
 
 export const PlacesPage = () => {
   useEffect(() => {
@@ -9,24 +10,22 @@ export const PlacesPage = () => {
   return (
     <div className="places-page">
       <h2>Places page</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
-        laboriosam labore aut iure odit maxime!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
-        quaerat architecto deserunt natus sequi corrupti! Voluptatum, soluta
-        similique ducimus reiciendis molestiae obcaecati esse fuga officiis
-        aperiam sequi earum, corrupti quis voluptas commodi iure ab dicta?
-        Aliquam voluptas dolorem, labore, fuga, at animi eveniet vitae eius
-        alias libero consectetur quod eaque impedit natus doloremque! Facere
-        quidem temporibus, corrupti qui consectetur veritatis dicta magnam
-        maxime hic, quam earum optio asperiores eos dolorem porro at architecto
-        recusandae ipsum consequuntur aperiam voluptatibus beatae praesentium
-        libero. Nostrum sequi, natus consequatur dolore accusamus quaerat
-        laboriosam numquam delectus facilis. Praesentium cum itaque officiis
-        unde quisquam quis iure.
-      </p>
+      <div className="gallery__container">
+        <div className="gallery__card">
+          <img src={SwedenImg} alt="A street in Stockholm" />
+          <span className="country-title">Sweden</span>
+        </div>
+
+        <div className="gallery__card">
+          <img src={SwedenImg} alt="A street in Stockholm" />
+          <span className="country-title">Czech Republic</span>
+        </div>
+
+        <div className="gallery__card">
+          <img src={SwedenImg} alt="A street in Stockholm" />
+          <span className="country-title">Denmark</span>
+        </div>
+      </div>
     </div>
   );
 };
